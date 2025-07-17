c7640153d3ac04e78cce7084b059daa7106cef8e
asraful0106 <asrafulapf@gmail.com>
2025-07-17 16:29:00 +0600

    createUser service has been updated

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,29 @@
-import { IUser } from "./user.interface";
+import httpStatusCode from 'http-status-codes';
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import { envVars } from '../../config/env';
+import bcrypt from "bcryptjs";
+import { string } from 'zod';
 
 const createUser = async(paylod: Partial<IUser>) =>{
-    const {name, email} = paylod;
-    const user = await User.create({name, email});
+    const {email, password, ...rest} = paylod;
+    
+    const isUserExist = await User.findOne({email});
+
+    if(isUserExist){
+        throw new AppError(httpStatusCode.BAD_REQUEST, "User Already Exist!");
+    }
+
+    const hashedPassword = await bcrypt.hash(password as string, Number(envVars.BCRYPT_SALT_ROUND));
+    const authProvider: IAuthProvider = {provider: "credentials", providerId: email as string}
+
+    const user = await User.create({
+        email,
+        password: hashedPassword,
+        auths: [authProvider],
+        ...rest
+    });
     return user;
 };
 
