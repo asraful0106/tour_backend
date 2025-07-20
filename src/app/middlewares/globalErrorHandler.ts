fccb4fa38da8d70637090887a02a74ee495c77ec
asraful0106 <asrafulapf@gmail.com>
2025-07-21 01:41:35 +0600

    error hndle applyed

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,13 +1,50 @@
 import { NextFunction, Request, Response } from "express";
 import AppError from "../errorHelpers/AppError";
 import { envVars } from "../config/env";
+import { TErrorSourch } from "../interfaces/error.types";
+import { handleDuplicateError } from "../helpers/handleDuplicateError";
+import { handleCastError } from "../helpers/handleCastErrors";
+import { handleZoodError } from "../helpers/handleZoodError";
+import { handleValidationError } from "../helpers/handleValidationError";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    let statusCode = 500;
-    let message = "Something Went Wrong!";
 
-    if(err instanceof AppError) {
+    if (envVars.NODE_ENV === "Development") {
+        // eslint-disable-next-line no-console
+        console.log(err);
+    }
+
+    let errorSources: TErrorSourch[] = []
+    let statusCode = 500
+    let message = "Something Went Wrong!!"
+
+    //Duplicate error
+    if (err.code === 11000) {
+        const simplifiedError = handleDuplicateError(err)
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message
+    }
+    // Object ID error / Cast Error
+    else if (err.name === "CastError") {
+        const simplifiedError = handleCastError(err)
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message
+    }
+    else if (err.name === "ZodError") {
+        const simplifiedError = handleZoodError(err)
+        statusCode = simplifiedError.statusCode
+        message = simplifiedError.message
+        errorSources = simplifiedError.errorSources as TErrorSourch[]
+    }
+    //Mongoose Validation Error
+    else if (err.name === "ValidationError") {
+        const simplifiedError = handleValidationError(err)
+        statusCode = simplifiedError.statusCode;
+        errorSources = simplifiedError.errorSources as TErrorSourch[]
+        message = simplifiedError.message
+    }
+    else if(err instanceof AppError) {
         statusCode = err.statusCode;
         message = err.message;
     }else if(err instanceof Error){
@@ -18,7 +55,8 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
     res.status(statusCode).json({
         success: false,
         message,
-        err,
+        errorSources,
+        err: envVars.NODE_ENV === "Development" ? err : null,
         stack: envVars.NODE_ENV === "Development" ? err.stack : null
     });
 }
--- src/app/util/catchAsync.ts
+++ src/app/util/catchAsync.ts
@@ -5,7 +5,7 @@ type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise
 export const catchAsync = (fn: AsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     Promise.resolve(fn(req, res, next)).catch((err: any) => {
-        console.log(err);
+        // console.log(err);
         next(err);
     })
 }
