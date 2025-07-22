1ef2eae92b9a77235a8411c7f9fa20867d49132a
asraful0106 <asrafulapf@gmail.com>
2025-07-22 10:04:39 +0600

    tour pre hook save and update has been done

--- src/app/modules/tour/tour.model.ts
+++ src/app/modules/tour/tour.model.ts
@@ -1,9 +1,9 @@
 import { model, Schema } from "mongoose";
 import { ITour, ITourType } from "./tour.interface";
 
-const tourTypeSchema = new Schema<ITourType> ({
-    name: {type: String, require: true, unique: true}
-},{
+const tourTypeSchema = new Schema<ITourType>({
+    name: { type: String, require: true, unique: true }
+}, {
     timestamps: true,
     versionKey: false
 });
@@ -11,21 +11,22 @@ const tourTypeSchema = new Schema<ITourType> ({
 export const TourType = model<ITourType>("TourType", tourTypeSchema);
 
 const tourSchema = new Schema<ITour>({
-    title: {type: String, required: true},
-    slug: {type: String, required: true, unique: true},
-    description: {type: String},
-    images: {type: [String], default: []},
-    location: {type: [String], default: []},
-    costFrom: {type: Number},
-    stratDate: {type: Date},
-    endDate: {type: Date},
-    included: {type: [String], default: []},
-    excluded: {type: [String], default: []},
-    amenities: {type: [String], default: []},
-    tourPlan: {type: [String], default: []},
-    maxGuest: {type: Number},
-    minAge: {type: Number},
-    division: {type: Schema.Types.ObjectId,
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    description: { type: String },
+    images: { type: [String], default: [] },
+    location: { type: [String], default: [] },
+    costFrom: { type: Number },
+    stratDate: { type: Date },
+    endDate: { type: Date },
+    included: { type: [String], default: [] },
+    excluded: { type: [String], default: [] },
+    amenities: { type: [String], default: [] },
+    tourPlan: { type: [String], default: [] },
+    maxGuest: { type: Number },
+    minAge: { type: Number },
+    division: {
+        type: Schema.Types.ObjectId,
         ref: "Division",
         required: true
     },
@@ -34,9 +35,46 @@ const tourSchema = new Schema<ITour>({
         ref: "TourType",
         required: true
     }
-},{
+}, {
     timestamps: true,
     versionKey: false
 });
+
+tourSchema.pre("save", async function (next) {
+
+    if (this.isModified("title")) {
+        const baseSlug = this.title.toLowerCase().split(" ").join("-")
+        let slug = `${baseSlug}`
+
+        let counter = 0;
+        while (await Tour.exists({ slug })) {
+            slug = `${slug}-${counter++}` // dhaka-division-2
+        }
+
+        this.slug = slug;
+    }
+    next()
+})
+
+tourSchema.pre("findOneAndUpdate", async function (next) {
+    const tour = this.getUpdate() as Partial<ITour>
+
+    if (tour.title) {
+        const baseSlug = tour.title.toLowerCase().split(" ").join("-")
+        let slug = `${baseSlug}`
+
+
+        let counter = 0;
+        while (await Tour.exists({ slug })) {
+            slug = `${slug}-${counter++}` // dhaka-division-2
+        }
+
+        tour.slug = slug
+    }
+
+    this.setUpdate(tour)
+
+    next()
+})
 
 export const Tour = model<ITour>("Tour", tourSchema);
